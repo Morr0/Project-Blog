7b9e169bee5f2a195747a13f964c9277fec757ce
Rami Hikmat <ramihikmat10@gmail.com>
2020-05-31 13:29:04 +1000

    Some navbar menu CSS improvement

--- clientNuxt/components/Navbar.vue
+++ clientNuxt/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <client-only placeholder="Loading...">
-    <nav class="flex items-center justify-between flex-wrap p-6 bg-gray-100 sticky">
-        <div class="flex items-center flex-shrink-0 text-gray-900 mr-6">
+    <nav class="grid grid-flow-col grid-cols-1 items-center justify-between flex-wrap p-6 bg-gray-100 sticky">
+        <div class="w-1/2 flex items-center flex-shrink-0 text-gray-900 mr-6">
             <!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
             <!-- TODO Make the logo glow on hover -->
             <!-- <img src="@/assets/logo.png" alt=".png" class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" /> -->
@@ -9,7 +9,7 @@
             <span class="font-semibold text-2xl tracking-tight"><nuxt-link to="/">{{this.$store.state.title}}</nuxt-link></span>
             <Icon :githubLogo="true" />
         </div>
-        <div class="w-full block flex-grow sm:flex sm:flex-row-reverse sm:flex-grow-0 sm:items-center sm:w-auto">
+        <div class="block flex-grow sm:flex sm:flex-row-reverse flex-wrap justify-center sm:flex-grow-0 sm:items-center sm:w-auto">
             <div class="text-lg items-end lg:flex-grow">
                 <nuxt-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-blue-500 mr-4 font-bold">
                     Home
@@ -26,10 +26,10 @@
                 <a href="" class="mr-4 text-gray-700 hover:text-blue-500 w-12 relative drop font-bold">
                     Apps
                     <div class="dropdownMenu" >
-                        <li><a target="_blank" href="https://morr0.github.io/Pomodoro-Timer/" 
+                        <li title="Pomodoro Timer"><a target="_blank" href="https://morr0.github.io/Pomodoro-Timer/" 
                         class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
                             Pomodoro Timer</a></li>
-                        <li><a target="_blank" href="https://morr0.github.io/Rosary-Prayer-Counter/" 
+                        <li title="Rosary App"><a target="_blank" href="https://morr0.github.io/Rosary-Prayer-Counter/" 
                         class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
                             Rosary App</a></li>
                     </div>
