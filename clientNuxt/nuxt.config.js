5d5af45192260bfbc0b06dc3c5421f9b1940219d
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-04 13:52:24 +1000

    Towards better SEO

--- clientNuxt/nuxt.config.js
+++ clientNuxt/nuxt.config.js
@@ -5,11 +5,14 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: "Rami Hikmat's",
+    title: "Rami Hikmat's Blog",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "This is a personal site where I host my portfolio and blog. My name is Rami Hikmat." }
+      { hid: 'description', name: 'description', content: "This is a personal site where I host my portfolio and blog. My name is Rami Hikmat." },
+      { hid: "og:title", name: "og:title", content: "Rami Hikmat's Blog"},
+      { hid: "og:url", name: "og:title", content: "https://ramihikmat.net"},
+      { hid: "og:description", name: "og:title", content: "This is a personal site where I host my portfolio and blog. My name is Rami Hikmat."},
     ],
     link: [
       { rel: 'shortcut icon', href: "/favicon.ico" }
