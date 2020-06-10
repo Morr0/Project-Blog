af4b3ebd160a876f82a66e8fcb6c8d31770f4cd1
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-10 20:10:04 +1000

    Server code cleanup #2

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,25 +5,7 @@ const models = require("../models/DBModels");
 
 const uuid = require("uuid").v1;
 
-// TODO Error handling
-
 route.get("/", (req, res) => {
-    // models.Post.find({
-    //     draft: false,
-    //     hidden: false
-    // }, (error, data) => {
-    //     if (error) return res.status(500).end();
-    //     if (!data) return res.status(404).end();
-
-    //     // only send needed stuff for better network performance i.e. ignoring the description
-    //     data.forEach((element) => {
-    //         element.description = "";
-    //     });
-    //     return res.status(200).json(data);
-    // }).sort({postDate: "desc"});
-
-    // return res.json(models.Post.get({_id: "5eafe2b465a90333abd042fa", "postDate": "x"}));
-
     const {
         amount = 4, // Amount of posts to display
     } = req.headers;
@@ -44,18 +26,6 @@ route.get("/", (req, res) => {
 });
 
 route.get("/:id", async (req, res) => {
-    // models.Post.findById(req.params.id, (error, post) => {
-    //     if (error || !post) return res.status(404).end();
-
-    //     // Whether the requester is allowed to view this page
-    //     if (post.draft || post.hidden){
-    //         if (!req.session.userId) return res.status(401).end();
-    //         else if (req.session.userId != post.author) return res.status(401).end();
-    //     }
-
-    //     return res.status(200).json(post);
-    // });
-
     const post = await models.Post.get({_id: req.params.id});
     if (!post) return res.status(404).end();
 
@@ -68,24 +38,6 @@ route.get("/:id", async (req, res) => {
 });
 
 route.get("/user/:id", (req, res) => {
-    // const callback = (error, data) => {
-    //     if (error) return res.status(500).end();
-    //     if (!data) return res.status(404).end();
-
-    //     // only send needed stuff for better network performance i.e. ignoring the description
-    //     data.forEach((element) => {
-    //         element.description = "";
-    //     });
-    //     return res.status(200).json(data);
-    // };
-
-    // if (req.session.userId && req.session.userId === req.params.id){
-    //     models.Post.find({author: req.params.id}, callback).sort({postDate: "desc"});
-    // } else { // Return the non-hidden non-draft work of the user (publicly available posts)
-    //     models.Post.find({author: req.params.id, draft: false, hidden: false}, callback).sort({postDate: "desc"});
-    // }
-
-    // let data = await models.Post.batchGet({_id: req.params.id});
     // Bear in mind this does only retrieve my user's items
     models.Post.scan().exec((error, data) => {
         if (req.session.userId && req.session.userId === req.params.id){ // If authorised to see all the user's documents when logged in
@@ -113,22 +65,6 @@ function checkLoggedIn(req, res, next){
 }
 
 route.post("/", checkLoggedIn, (req, res) => {
-    // const newArticle = new models.Post({
-    //     author: req.session.userId,
-    //     title: req.headers.title,
-    //     description: req.headers.description,
-    //     content: req.body,
-    //     draft: req.headers.draft,
-    //     hidden: req.headers.hidden,
-    // });
-    // newArticle.save((error, data) => {
-    //     if (error) return res.status(500).end();
-         
-    //     return res.status(200).json(data._id);
-    // });
-
-    // req.params.draft = req.params.draft? true: false;
-    // req.params.hidden = req.params.hidden? true: false;
     const newArticle = {
         _id: uuid(),
         author: req.session.userId,
@@ -150,7 +86,7 @@ route.post("/", checkLoggedIn, (req, res) => {
 });
 
 route.put("/:id", checkLoggedIn, (req, res) => {
-    // Things that are to be updates, checks if they were included in HTTP header to be updated
+    // Things that are to be updated, checks if they were included in HTTP header to be updated
 
     console.log(`hidden: ${req.headers.hidden} and type: ${typeof req.headers.hidden}`);
     
@@ -161,14 +97,6 @@ route.put("/:id", checkLoggedIn, (req, res) => {
     if (req.headers.hidden) toBeUpdated.hidden = stringToBoolean(req.headers.hidden);
     if (req.headers.draft) toBeUpdated.draft = stringToBoolean(req.headers.draft);
 
-    // if (!req.headers.draft && !req.headers.hidden) toBeUpdated.postDate = String(Date.now());
-
-    // models.Post.findByIdAndUpdate(req.params.id, toBeUpdated, (error) => {
-    //     if (error) return res.status(500).end();
-            
-    //     res.status(200).end();
-    // });
-
     models.Post.update({_id: req.params.id}, toBeUpdated, (error, post) => {
         if (error) return res.status(500).end();
             
@@ -177,14 +105,6 @@ route.put("/:id", checkLoggedIn, (req, res) => {
 });
 
 route.delete("/:id", checkLoggedIn, (req, res) => {
-    // models.Post.findByIdAndRemove(req.params.id, (error, post) => {
-    //     if (error) return res.status(500).end();
-
-    //     if (!post) return res.status(400).end();
-
-    //     return res.status(200).end();
-    // });
-
     models.Post.delete({_id: req.params.id}, (error, post) => {
         if (error) return res.status(500).end();
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -13,8 +13,6 @@ const s3 = new aws.S3({
     secretAccessKey: process.env.AWS_PRIVATE_KEY,
 });
 
-// TODO Error Handling
-
 const uploader = multer({
     storage: multerS3({
         s3: s3,
@@ -41,8 +39,10 @@ const uploader = multer({
 
 const bcrypt = require("bcrypt");
 
+// REGISTERATION STUFF ONLY -- BEGIN
 const salt = 11;
 const ALLOWED_TO_REGISTER_USERS = true;
+// END
 
 const models = require("../models/DBModels");
 
@@ -56,20 +56,15 @@ function checkLoggedIn (req, res, next){
 }
 
 route.get("/loggedIn", async (req, res) => {
-    // if (!req.session) return res.status(404).end();
     if (!req.session.userId) return res.status(404).end();
 
     const data = await models.User.get({_id: req.session.userId});
     data.email = data.password = undefined;
     return res.status(200).json({id: data._id, name: data.name});
-    // models.User.findById(req.session.userId, (error, data) => {
-    //     if (error) return res.status(500).end();
-
-    //     return res.status(200).json({id: data._id, name: data.name});
-    // });
 });
 
-// Currently not registering
+/*
+// CURRENTLY NOT REGISTERING
 // route.get("/allowedToRegister", (req, res) => {
 //     if (!req.session.userId) return res.status(ALLOWED_TO_REGISTER_USERS? 200: 401).end();
 
@@ -107,28 +102,9 @@ route.get("/loggedIn", async (req, res) => {
 //         });
 //     } else return res.status(400).end();
 // });
+*/
 
 route.post("/login", checkLoggedIn, async (req, res) => {
-    // if (req.headers.email && req.headers.password){
-    //     models.User.findOne({email: req.headers.email}, (error, user) => {
-    //         if (error) return res.status(500).end();
-    //         // Password or email incorrect
-    //         if (!user) return res.status(404).end();
-
-    //         else {
-    //             bcrypt.compare(req.headers.password, user.password, (error, same) => {
-    //                 if (!error){
-    //                     // If login credentials are correct
-    //                     if (same){
-    //                         req.session.userId = user._id;
-    //                         res.status(202).json({id: user._id, name: user.name});
-    //                     } else return res.status(404).end();
-    //                 } else return res.status(500).json({error: error});
-    //             });
-    //         }
-    //     });
-    // } else return res.status(400).json({error: "Incomplete request"});
-
     if (req.headers.email && req.headers.password){
         models.User.scan().filter("email").eq(req.headers.email).exec((error, user) => {
             if (error) return res.status(500).end();
@@ -169,17 +145,6 @@ route.post("/logout", (req, res) => {
 });
 
 route.get("/:id", async (req, res) => {
-    // models.User.findById(req.params.userId, (error, data) => {
-    //     if (error) return res.status(500).end();
-    //     if (!data) return res.status(404).end();
-        
-    //     else {
-    //         if (!data) return res.status(404).end();
-
-    //         return res.status(200).json(data);
-    //     }
-    // });
-
     const user = await models.User.get({_id: req.params.id});
     if (!user) return res.status(404).end();
 
@@ -189,12 +154,6 @@ route.get("/:id", async (req, res) => {
 });
 
 route.put("/image/profile/:id", uploader.single("picture"), async (req, res) => {
-    // Updating the database with the url of the image
-    // models.User.findByIdAndUpdate(req.session.userId, {image_url: req.file.location}, (error, data) => {
-    //     if (error) return res.status(500);
-
-    //     return res.status(200).end();
-    // });
     if (req.session.userId !== req.params.id) return res.status(401).end();
 
     const user = await models.User.update({_id: req.params.id});
@@ -204,16 +163,6 @@ route.put("/image/profile/:id", uploader.single("picture"), async (req, res) => 
 });
 
 route.put("/:id", checkLoggedIn, async (req, res) => {
-//     console.log("Called");
-//     if (req.session.userId !== req.params.userId) return res.status(401).end();
-
-//     console.log("PAST HERE");
-//     models.User.findByIdAndUpdate(req.params.userId, req.body.user, (error, data) => {
-//         if (error) return res.status(500).end();
-
-//         return res.status(200).json(data);
-//     });
-
     if (req.session.userId !== req.params.id) return res.status(401).end();
 
     const user = await models.User.update({_id: req.params.id}, req.body.user);
