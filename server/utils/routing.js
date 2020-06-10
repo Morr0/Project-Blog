67c10d8628c8eb03886ae70efa176e46d62f5906
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-10 20:05:24 +1000

    Bad request/ error handling

--- server/utils/routing.js
+++ server/utils/routing.js
@@ -4,4 +4,14 @@ const postsRouter = require("../routes/posts");
 module.exports = function (app){
     app.use("/users", usersRouter);
     app.use("/posts", postsRouter);
+
+    // Incorrect URL handler
+    app.use((req, res) => {
+        return res.status(400).end();
+    });
+
+    app.use((err, req, res, next) => {
+        console.error(err);
+        return res.status(500).end();
+    })
 }
