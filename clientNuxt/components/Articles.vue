0d4d67a2b38d5a2b46fc11c880d344cacb55f752
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-16 13:14:40 +1000

    Deciding on CSS

--- clientNuxt/components/Articles.vue
+++ clientNuxt/components/Articles.vue
@@ -2,6 +2,9 @@
     <div class="container" v-if="posts">
         <Article v-for="(post) in posts" :post="post" :key="post._id" :individualPage="false" :minimised="minimised" />
     </div>
+    <div v-else class="">
+        No posts ...
+    </div>
 </template>
 
 <script>
