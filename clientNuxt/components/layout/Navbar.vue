081e3d16dd597d205bccc6af672fd3ad87488981
Rami Hikmat <ramihikmat10@gmail.com>
2020-06-17 13:17:59 +1000

    Complete revamp of Navbar

--- clientNuxt/components/layout/Navbar.vue
+++ clientNuxt/components/layout/Navbar.vue
@@ -1,93 +1,55 @@
 <template>
-  <!-- <client-only placeholder="Loading..."> -->
-    <!-- <nav class="flex items-center justify-between flex-wrap p-6 bg-gray-100 sticky">
-        <div class="flex items-center flex-shrink-0 text-gray-900 mr-6">
-            <Icon :logo="true" />
-            <span class="font-extrabold text-3xl text-gray-800 hover:text-blue-500 tracking-tight"><nuxt-link to="/">Rami Hikmat's</nuxt-link></span>
-        </div>
-        <div class="flex flex-grow sm:flex sm:flex-row-reverse flex-wrap justify-center sm:flex-grow-0 sm:items-center sm:w-auto">
-            <div class="text-lg items-end lg:flex-grow">
-
-                <a class="mr-4 text-gray-700 hover:text-blue-500 w-12 relative drop font-bold">
-                    Apps
-                    <div class="dropdownMenu" >
-                        <li title="Pomodoro Timer"><a target="_blank" href="https://morr0.github.io/Pomodoro-Timer/" 
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Pomodoro Timer</a></li>
-                        <li title="Rosary App"><a target="_blank" href="https://morr0.github.io/Rosary-Prayer-Counter/" 
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Rosary App</a></li>
-                    </div>
-                </a>
-
-                <a class="mr-4 text-gray-700 hover:text-blue-500 w-12 relative drop font-bold">
-                    Me
-                    <div class="dropdownMenu" >
-                        <li title="Portfolio"><a target="_blank" href="https://ramihikmat.net" 
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Portfolio</a></li>
-                        <li title="Pomodoro Timer"><a target="_blank" href="https://www.linkedin.com/in/rami-hikmat/" 
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Linkedin</a></li>
-                        <li title="Rosary App"><a target="_blank" href="https://github.com/Morr0" 
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Github</a></li>
-                        <li title="Portfolio"><nuxt-link to="/askcontact/"
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Ask/Contact</nuxt-link></li>
-                        <li title="Pomodoro Timer"><nuxt-link to="/feedback/"
-                        class="px-4 py-2 block text-black hover:bg-grey-light text-gray-700 hover:text-blue-500">
-                            Give Feedback</nuxt-link></li>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </nav> -->
-  <!-- </client-only> -->
-
   <div class="">
       <v-app-bar>
-          <v-toolbar-title>Rami Hikmat</v-toolbar-title>
+          <v-toolbar-title>
+              <v-btn text x-large href="/">Rami Hikmat</v-btn>
+          </v-toolbar-title>
           <v-spacer></v-spacer>
 
           <v-menu open-on-hover offset-y>
               <template v-slot:activator="{ on, attrs }">
                 <v-btn
-                color="primary"
-                dark
                 v-bind="attrs"
                 v-on="on"
+                text
                 >
                 Apps
                 </v-btn>
               </template>
-              <v-list>
+              <v-list column>
                   <v-list-item>
-                      <v-list-item-title>Hello world</v-list-item-title>
+                      <v-btn text href="https://morr0.github.io/Pomodoro-Timer/" target="_blank">
+                          Pomodoro Timer
+                      </v-btn>
+                  </v-list-item>
+                  <v-list-item>
+                      <v-btn text href="https://morr0.github.io/Rosary-Prayer-Counter/" target="_blank">
+                          Rosary Prayer
+                      </v-btn>
+                  </v-list-item>
+                  <v-list-item>
+                      <v-btn text href="https://play.google.com/store/apps/details?id=com.project.utilitarian&hl=en_US" target="_blank">
+                          Project Utility
+                      </v-btn>
+                  </v-list-item>
+                  <v-list-item>
+                      <v-btn text href="https://play.google.com/store/apps/details?id=rami.project.orange&hl=en_US" target="_blank">
+                          Cram List
+                      </v-btn>
                   </v-list-item>
               </v-list>
           </v-menu>
 
 
-          <v-btn icon>
+          <v-btn icon href="https://www.ramihikmat.net" target="_blank">
             <v-icon>mdi-account-circle</v-icon>
           </v-btn>
-          <v-btn icon>
+          <v-btn icon href="https://www.linkedin.com/in/rami-hikmat/" target="_blank">
             <v-icon>mdi-linkedin</v-icon>
           </v-btn>
-          <v-btn icon>
+          <v-btn icon href="https://github.com/Morr0" target="_blank">
             <v-icon>mdi-github</v-icon>
           </v-btn>
       </v-app-bar>
   </div>
 </template>
-
-<script>
-import Icon from "@/components/Icon.vue";
-
-export default {
-    components: {
-        Icon,
-    },
-}
-</script>
