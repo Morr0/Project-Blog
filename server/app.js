352351b04bbc4a88902bf8c623ce1d17a2effb28
Rami Hikmat <ramihikmat10@gmail.com>
2020-08-04 22:23:40 +1000

    Preparing to lambda #3

---
+++ server/app.js
@@ -0,0 +1,31 @@
+module.exports = function (){
+    // Imports
+    const express = require("express");
+
+    // Inits
+    require("dotenv").config();
+    const app = express();
+
+    // Constants
+    const env = {
+        NODE_ENV = "production",
+        PORT = 3400,
+    } = process.env;
+
+
+    // To facilitate production frontend from development's
+    console.log(NODE_ENV);
+    let ORIGIN = "http://localhost:3000";
+    if (NODE_ENV === "production"){
+        ORIGIN = JSON.parse(process.env.ORIGIN);
+    } 
+    console.log(`CORS: ${ORIGIN}`);
+
+    // Initial DB Connection MUST KEEP
+    require("./utils/db/dbconnection");
+
+    app.listen(PORT);
+
+    require("./utils/middlewares")(app, env); // Middleware handling
+    require("./utils/routing")(app); // Routing and error handling
+}
